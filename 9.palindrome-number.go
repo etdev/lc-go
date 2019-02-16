28c47fa0c6c2becef652ff68d7143278d561bf43
Eric Turner <ericturnerdev@gmail.com>
2019-02-16 19:14:00 +0900

    Add palindrome-number

--- 9.palindrome-number.go
+++ 9.palindrome-number.go
@@ -1,3 +1,20 @@
+package main
+
+import "fmt"
+
 func isPalindrome(x int) bool {
-    
+	xStr := fmt.Sprintf("%d", x)
+	return reverseStr(xStr) == xStr
+}
+
+func reverseStr(str string) string {
+	runes := []rune(str)
+
+	for i := 0; i < len(runes)/2; i++ {
+		bak := runes[i]
+		runes[i] = runes[len(runes)-1-i]
+		runes[len(runes)-1-i] = bak
+	}
+
+	return string(runes)
 }
