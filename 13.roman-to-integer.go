78d2030cdf5a0b622dabd449f6fe4956ba4af3a4
Eric Turner <ericturnerdev@gmail.com>
2019-02-16 22:14:57 +0900

    Refactor roman-to-integer

--- 13.roman-to-integer.go
+++ 13.roman-to-integer.go
@@ -4,31 +4,35 @@ import (
 	"strings"
 )
 
+type mapping struct {
+	code  string
+	value int
+}
+
 func romanToInt(s string) int {
-	mappings := map[string]int{
-		"M":  1000,
-		"CM": 900,
-		"D":  500,
-		"CD": 400,
-		"C":  100,
-		"XC": 90,
-		"L":  50,
-		"XL": 40,
-		"X":  10,
-		"IX": 9,
-		"V":  5,
-		"IV": 4,
-		"I":  1,
+	mappings := []mapping{
+		{"M", 1000},
+		{"CM", 900},
+		{"D", 500},
+		{"CD", 400},
+		{"C", 100},
+		{"XC", 90},
+		{"L", 50},
+		{"XL", 40},
+		{"X", 10},
+		{"IX", 9},
+		{"V", 5},
+		{"IV", 4},
+		{"I", 1},
 	}
 
-	letterGroups := []string{"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"}
 	sum := 0
 	runes := []rune(s)
 
-	for _, letters := range letterGroups {
-		for strings.HasPrefix(string(runes), letters) {
-			sum += mappings[letters]
-			runes = append(runes[:0], runes[len(letters):]...)
+	for _, mapping := range mappings {
+		for strings.HasPrefix(string(runes), mapping.code) {
+			sum += mapping.value
+			runes = append(runes[:0], runes[len(mapping.code):]...)
 		}
 	}
 
