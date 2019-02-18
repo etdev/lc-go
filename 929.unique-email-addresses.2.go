8f77729bdd23630a6dc18eafa6fe2ebcd5fb6b93
Eric Turner <ericturnerdev@gmail.com>
2019-02-18 22:47:02 +0900

    Update

--- 929.unique-email-addresses.2.go
+++ 929.unique-email-addresses.2.go
@@ -1,24 +1,17 @@
 import (
-	"fmt"
 	"strings"
 )
 
 func numUniqueEmails(emails []string) int {
 	seen := map[string]bool{}
-	count := 0
 
 	for _, email := range emails {
 		split := strings.Split(email, "@")
-		username, domain := split[0], split[1]
-		username = strings.Replace(username, ".", "", -1)
+		username := strings.Replace(split[0], ".", "", -1)
 		username = strings.Split(username, "+")[0]
-		cleaned := fmt.Sprintf("%s%s", username, domain)
-
-		if !seen[cleaned] {
-			count++
-			seen[cleaned] = true
-		}
+		domain := split[1]
+		seen[strings.Join([]string{username, domain}, "")] = true
 	}
 
-	return count
+	return len(seen)
 }
